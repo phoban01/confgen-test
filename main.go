086b5e12981942d9cfb7fba2bd0f79ddfa5bb603
Piaras Hoban <phoban01@gmail.com>
2023-09-17 15:36:58 +0100

    working
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

---
+++ main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/ast"
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/format"
+	"cuelang.org/go/cue/parser"
+	"golang.org/x/exp/slices"
+)
+
+// TODO: prune fields not in schema that are in data file
+// Run cue fmt or equivelant to simplify and ensure consistent formatting
+// TODO: ensure comments are always set from schema
+
+func main() {
+	inputDataFile, err := filepath.Abs("data.cue")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := cuecontext.New()
+
+	schema, schemaComments, err := parseFile(ctx, "schema.cue")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, _, err := parseFile(ctx, inputDataFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	missingFields := make([]cue.Path, 0)
+	missingFields, err = getMissingFields(schema, data, missingFields)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	completed := &ast.File{Decls: generateDefaults(schema, missingFields)}
+	completed.SetComments(schemaComments)
+	newData := ctx.BuildFile(completed, cue.Scope(data))
+
+	ufx := data.Unify(newData)
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpFile, err := os.CreateTemp(cwd, "mpas-data-*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exportFile := &ast.File{
+		Decls: ufx.Syntax(cue.Docs(true), cue.All()).(*ast.StructLit).Elts,
+	}
+	exportFile.SetComments(schemaComments)
+	exportBytes, err := format.Node(exportFile, format.Simplify())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(exportBytes))
+
+	if _, err := tmpFile.Write(exportBytes); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := tmpFile.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := os.Rename(tmpFile.Name(), inputDataFile); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parseFile(ctx *cue.Context, p string) (cue.Value, []*ast.CommentGroup, error) {
+	tree, err := parser.ParseFile(p, nil, parser.ParseComments)
+	if err != nil {
+		return cue.Value{}, nil, err
+	}
+
+	return ctx.BuildFile(tree), tree.Comments(), nil
+}
+
+func generateDefaults(input cue.Value, missingFields []cue.Path) []ast.Decl {
+	result := make([]ast.Decl, 0)
+	fields, err := input.Fields(cue.All())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var paths []string
+	for _, p := range missingFields {
+		paths = append(paths, p.String())
+	}
+
+	result = makeValues(fields, paths, result)
+	return result
+}
+
+func makeValues(
+	i *cue.Iterator,
+	paths []string,
+	result []ast.Decl,
+	parents ...cue.Selector,
+) []ast.Decl {
+	for i.Next() {
+		var value ast.Expr
+		var required bool
+		v := i.Value()
+		label, _ := v.Label()
+		sel := append(parents, i.Selector())
+		path := cue.MakePath(sel...)
+		comments := v.Doc()
+
+		if !slices.Contains(paths, path.String()) {
+			continue
+		}
+
+		field, hasDefaultValue := v.Default()
+
+		if !hasDefaultValue && v.IsConcrete() {
+			switch v.Syntax(cue.Raw()).(type) {
+			case *ast.StructLit:
+				var rx []ast.Decl
+				f, err := i.Value().Fields()
+				if err != nil {
+					log.Fatal(err)
+				}
+				rx = makeValues(f, paths, rx, sel...)
+				value = &ast.StructLit{
+					Elts: rx,
+				}
+			default:
+				value = field.Syntax(cue.Raw()).(ast.Expr)
+			}
+		} else if !hasDefaultValue {
+			required = true
+		} else {
+			value = field.Syntax(cue.Raw()).(ast.Expr)
+		}
+
+		if value != nil && !required {
+			f := &ast.Field{
+				Label: ast.NewIdent(label),
+				Value: value,
+			}
+			ast.SetComments(f, comments)
+			result = append(result, f)
+		} else if value == nil {
+			f := &ast.Field{
+				Label: ast.NewIdent(label),
+			}
+			switch v.Syntax().(type) {
+			case *ast.BinaryExpr:
+				f.Value = v.Syntax(cue.Raw()).(*ast.BinaryExpr)
+			default:
+				f.Value = ast.NewString(fmt.Sprintf("<%s>", v.IncompleteKind()))
+			}
+			ast.SetComments(f, comments)
+			result = append(result, f)
+		}
+	}
+	return result
+}
+
+func getMissingFields(
+	c cue.Value,
+	v cue.Value,
+	m []cue.Path,
+	parents ...cue.Selector,
+) ([]cue.Path, error) {
+	i, err := c.Fields(cue.All())
+	if err != nil {
+		return nil, err
+	}
+
+	for i.Next() {
+		sel := append(parents, i.Selector())
+		path := cue.MakePath(sel...)
+		if !v.LookupPath(path).Exists() {
+			m = append(m, path)
+		}
+
+		switch i.Value().Syntax().(type) {
+		case *ast.StructLit:
+			// make a copy to avoid iterator issues
+			x := c.LookupPath(path)
+
+			// iterate over the struct fields, dropping the
+			// prefix which is the name of the struct
+			var n []cue.Path
+			n, err = getMissingFields(x, v, n, sel[1:]...)
+			if err != nil {
+				return nil, err
+			}
+
+			// restore the selector prefix to the path
+			for _, nv := range n {
+				nsel := sel[:]
+				for _, nvs := range nv.Selectors() {
+					nsel = append(nsel, nvs)
+				}
+				m = append(m, cue.MakePath(nsel...))
+			}
+		}
+	}
+
+	return m, nil
+}
--- schema.cue
+++ schema.cue
@@ -2,56 +2,63 @@
 // that introduces the project.
 //
 
+// sets the replicas for the service
+replicas!: *2 | int
+
+// replicas max
+max: replicas * 3
+
 // new value
-owner: "<string>"
+owner!: string & =~".+@sap.com"
 
 // counter value
-counter: "<int>"
+counter!: int
 
 // should tls be enabled for the service 
-enableTLS: true
+enableTLS: *true | bool
 
 // this is a required field with of type string with a constraint
-redis_url: "<string>"
+redis_url!: string & =~"^https://.+:6379$"
 
 // gate is an endpoint that will be checked at the given interval 
-gate: {
+gate!: {
 	// url 
 	url: "test"
 
 	// interval in miliseconds
-	interval: 1000
+	interval: *1000 | int
 
 	// data
 	data: {
 		// forename
-		forname: "piaras"
+		forname: *"piaras" | string
 		// surname
-		surname: "<string>"
+		surname: string
 
 		// address
 		address: {
 			// home place
-			home: "<string>"
+			home: string
 
 			// phone number
-			phone: "087 2853692"
+			phone: string
 		}
 	}
+
 }
 
 // this is an optional field
-labels: {}
+labels?: {[string]: string}
 
 // annotations can be added optionally
-annotations: {}
+annotations?: {[string]: {}}
 
-// sets the replicas for the service
-replicas: 2
+annotations: {
+	[string]: {
+		age: int
+	}
+}
 
 // count is a series of numbers
 // that have very little relevance to anyone
 count: [1, 2, 3, 4, 5]
-
-// replicas max
-max: replicas * 3
